234e1239b88c9aa8e6a29ffb85e6650a62850f4e
Tien Duc Le <6162574+tienducle@users.noreply.github.com>
2024-04-04 19:51:40 +0200

    Various fixes

--- create-nexus-archives-from-tag.sh
+++ create-nexus-archives-from-tag.sh
@@ -14,7 +14,7 @@ find . -name ".DS_Store" -exec rm {} \;
 rm -rf tmp
 mkdir tmp
 rm -rf tmp/*.zip
-cp "nsis/ridgeside-village-german-translation-installer.exe" "./tmp/ridgeside-village-german-translation-installer.exe"
+cp "nsis/ridgeside-village-german-translation-installer.zip" "./tmp/ridgeside-village-german-translation-installer.zip"
 zip -r "tmp/Ridgeside Village.zip" "Ridgeside Village" "Ridgeside Village - German Translation" -x "*default.json"
 
 git checkout main
